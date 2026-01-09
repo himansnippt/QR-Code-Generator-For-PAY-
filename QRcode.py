1fbd2435a144d6d0ff35c12088bf3dc8bc3aa84b
Himanshu Mishra <himanshumishra.9871@gmail.com>
2026-01-09 20:27:52 +0530

    Add files via upload

---
+++ QRcode.py
@@ -0,0 +1,23 @@
+import qrcode
+
+upi_id = input("Enter your UPI ID = ")
+
+# Defining payment URLs
+phonepe_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+paytm_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+google_pay_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+
+# Create QR Codes
+phonepe_qr = qrcode.make(phonepe_url)
+paytm_qr = qrcode.make(paytm_url)
+google_pay_qr = qrcode.make(google_pay_url)
+
+# Save QR codes
+phonepe_qr.save('phonepe_qr.png')
+paytm_qr.save('paytm_qr.png')
+google_pay_qr.save('google_pay_qr.png')
+
+# Show QR codes
+phonepe_qr.show()
+paytm_qr.show()
+google_pay_qr.show()
